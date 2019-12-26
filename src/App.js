df2c9b2785e2425a343bf7fa55711530e34d7e96
Jaime Morais <jaime.morais@serpro.gov.br>
2019-12-26 13:42:20 -0300

    Configure component folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import DividendAlertMenu from './DividendAlertMenu'
+import DividendAlertMenu from './components/DividendAlertMenu'
 
 function App() {
   return (
     <div className="App">
       <header >
         <p>
-          Dividend Alert React
+          Dividend Alert
         </p>        
       </header>
       
