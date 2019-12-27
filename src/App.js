17a70251155b98f246d6490257dcbce094cfbe1e
Jaime Morais <jaimemorais@gmail.com>
2019-12-27 13:06:00 -0300

    Implementing menu

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import DividendAlertMenu from './components/DividendAlertMenu'
 
 function App() {
   return (
-    <div className="App">
-      <header >
-        <p>
-          Dividend Alert
-        </p>        
-      </header>
+    <div className="App" id="App-Container">
+
+      <DividendAlertMenu pageWrapId={ "page-wrap" } outerContainerId={ "App-Container" }  />
+
+      <main id="page-wrap">      
+        <header >
+          <p>
+            Dividend Alert
+          </p>        
+        </header>    
+      </main>      
       
-      <DividendAlertMenu/>
 
     </div>
   );
