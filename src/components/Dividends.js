0e8effffd8efb45af11ac08c0f281dcd7c08e9b4
Jaime Morais <jaimemorais@gmail.com>
2020-01-01 14:07:35 -0300

    Add dividends component

---
+++ src/components/Dividends.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Dividends extends React.Component {
+  render () {
+    
+    return (
+      
+      <p>Dividends Component</p>
+
+    );
+  }
+}
+
+export default Dividends;
