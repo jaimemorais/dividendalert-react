dbcdef7b68b3277f7bca64ab377af97ab62543eb
Jaime Morais <jaimemorais@gmail.com>
2020-01-01 14:07:50 -0300

    Add MyStocks component

---
+++ src/components/MyStocks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MyStocks extends React.Component {
+  render () {
+    
+    return (
+      
+      <p>MyStocks Component</p>
+
+    );
+  }
+}
+
+export default MyStocks;
