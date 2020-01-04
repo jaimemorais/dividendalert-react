d4b3e27bee263ca8edd88dd2279d8e1877d49a9b
Jaime Morais <jaimemorais@gmail.com>
2020-01-04 12:31:59 -0300

    Fetch api data

--- src/components/Dividends.jsx
+++ src/components/Dividends.jsx
@@ -1,12 +1,56 @@
 import React from 'react';
 
 class Dividends extends React.Component {
+  
+
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+        isFetching: false,
+        dividends: []
+    };
+  }
+
+  componentDidMount() {
+      this.fetchDividends();
+      this.timer = setInterval(() => this.fetchDividends(), 15000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+    this.timer = null;
+  }
+
+
+  fetchDividends() {
+    this.setState({...this.state, isFetching: true});
+    console.log('url api : ' + process.env.REACT_APP_DIVIDENDALERT_API_URL);
+    fetch(process.env.REACT_APP_DIVIDENDALERT_API_URL)
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ dividends: data })
+    })
+    .catch(console.log)
+
+    this.setState({...this.state, isFetching: false});
+    
+    // TODO error handling       
+   }
+
+
   render () {
     
     return (
-      
-      <p>Dividends Component</p>
+      <div>
+      <p>Next Dividends</p>
 
+        {this.state.dividends.map((dividend) => (
+          <div>            
+              <h5>{dividend.stockName} - {dividend.type} - {dividend.paymentDate} - {dividend.value}</h5>            
+          </div>
+        ))}
+      </div>
     );
   }
 }
