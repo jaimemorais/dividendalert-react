e034eb4a301f76777c32405244b1be8e77364e86
Jaime Morais <jaimemorais@gmail.com>
2020-01-08 13:39:12 -0300

    Implementing api service

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 import { getToken } from "./authService";
 
-const api = axios.create({
+const dividendAlertApi = axios.create({
   baseURL: process.env.REACT_APP_DIVIDENDALERT_API_BASE_URL
 });
 
-api.interceptors.request.use(async config => {
+dividendAlertApi.interceptors.request.use(async config => {
   const token = getToken();
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
@@ -13,4 +13,4 @@ api.interceptors.request.use(async config => {
   return config;
 });
 
-export default api;
+export default dividendAlertApi;
