6019c6ebd4ed1db577f87abaa48890bd50f007fd
Jaime Morais <jaimemorais@gmail.com>
2020-01-08 13:27:48 -0300

    Add SelectStock component

---
+++ src/components/SelectStock.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactAutocomplete from 'react-autocomplete'
+
+class SelectStock extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      value: '',
+    };
+  }
+
+
+  render() {
+    return (
+      <ReactAutocomplete
+        items={[
+          { id: 'foo', label: 'ITSA3' },
+          { id: 'bar', label: 'WEGE3' },
+          { id: 'baz', label: 'EGIE3' },
+        ]}
+        shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
+        getItemValue={item => item.label}
+        renderItem={(item, highlighted) =>
+          <div
+            key={item.id}
+            style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
+          >
+            {item.label}
+          </div>
+        }
+        value={this.state.value}
+        onChange={e => this.setState({ value: e.target.value })}
+        onSelect={value => this.setState({ value })}
+      />
+    )
+  }
+  
+}
+
+export default SelectStock;
