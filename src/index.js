ad4404aa414b9ad0709b2d6273e224cef855b0bf
Jaime Morais <jaimemorais@gmail.com>
2020-06-19 07:54:28 -0300

    Change store name

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from "./redux/dividendAlertReduxStore";
+import dividendAlertReduxStore from "./redux/dividendAlertReduxStore";
 
 import * as serviceWorker from './serviceWorker';
 
@@ -11,7 +11,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import "font-awesome/css/font-awesome.css";
 
 ReactDOM.render(
-    <Provider store={store}>
+    <Provider store={dividendAlertReduxStore}>
         <App />
     </Provider>, 
     document.getElementById('root')
